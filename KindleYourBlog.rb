99cf3bf2727cfb2bd0753cedbf1cba8337ffee28
parkq <park@kyu.co>
2011-11-19 11:59:27 -0500

    added README

--- KindleYourBlog.rb
+++ KindleYourBlog.rb
@@ -3,23 +3,25 @@ require 'readability'
 require 'open-uri'
 require 'simple-rss'
 
-rss_url = 'http://www.instapaper.com/rss/497567/21ZkhzGjxw4LlB0gntEMr0R5E'
+rss_url = 'http://feeds.feedburner.com/GiantRobotsSmashingIntoOtherGiantRobots'
 
-def metadata
-  return "<metadata xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:opf=\"http://www.idpf.org/2007/opf\">
-    	<dc:title>Kindle User's Guide</dc:title>
+def metadata(title, date)
+  string = "<metadata xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:opf=\"http://www.idpf.org/2007/opf\">
+    	<dc:title>" + title + "</dc:title>
     	<dc:language>en-us</dc:language>
     	<meta name=\"cover\" content=\"My_Cover\" />
-  	<dc:identifier id=\"BookId\" opf:scheme=\"ISBN\">9781375890815</dc:identifier>
-  	<dc:creator>Kyu.co</dc:creator>
+  	<dc:identifier id=\"BookId\" opf:scheme=\"ISBN\"></dc:identifier>
+  	<dc:creator>Park@Kyu.co</dc:creator>
   	<dc:publisher></dc:publisher>
   	<dc:subject>Reference</dc:subject>
-  	<dc:date>2009-11-17</dc:date>
+  	<dc:date>" + date + "</dc:date>
   </metadata>"
+  return string
 end
 
 def item(name, loc)
-  return "<item id=\"" + name + "\" media-type=\"application/xhtml+xml\" href=\"" + loc + "\"></item>"
+  string = "<item id=\"" + name + "\" media-type=\"application/xhtml+xml\" href=\"" + loc + "\"></item>"
+  return string
 end
 
 def spine(filenames)
@@ -56,7 +58,12 @@ articles.each_with_index do |article, num|
   url = article.link
   source = open(url).read
   filenames.push "%04d-" % (num+1) + article.title + ".html"
-  readables.push "<body><h1>" +  article.title + "</h1>" + Readability::Document.new(source).content + "</body>"
+  string = "<body><h1>" +  article.title + "</h1>" + 
+    Readability::Document.new(source, :tags => %w[div p img a],  
+                                      :attributes => %w[src href], 
+                                      :remove_empty_nodes => false,
+                                      :debug => true).content + "</body>"
+  readables.push string
   aHTML = File.new(filenames.last, "w+:utf-8")
   aHTML.puts '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'
   aHTML.puts readables.last
@@ -67,7 +74,7 @@ end
 opf_filename = title + ".opf"
 bookdata = File.new(opf_filename, "w+:utf-8")
 bookdata.puts "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
-bookdata.puts metadata
+bookdata.puts metadata(title, date)
 bookdata.puts "<manifest>"
 filenames.each_with_index do |filename, num|
   bookdata.puts item("%04d" % (num+1), filename)
